4b3b1001a530370634ae7e5c05970609af58c6c0
onassis <onxssis@gmail.com>
2021-02-13 12:53:21 +0100

    update travis.yml

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+docker build -t onxssis/v1commerce_client:latest -t onxssis/v1commerce_client:$SHA -f ./client/Dockerfile ./client
+docker build -t onxssis/v1commerce_api:latest -t onxssis/v1commerce_api:$SHA -f ./api/Dockerfile ./api
+
+docker push onxssis/v1commerce_client:latest
+docker push onxssis/v1commerce_api:latest
+
+docker push onxssis/v1commerce_client:$SHA
+docker push onxssis/v1commerce_api:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/api-deployment api=onxssis/v1commerce_api:$SHA
+kubectl set image deployments/client-deployment client=onxssis/v1commerce_client:$SHA
