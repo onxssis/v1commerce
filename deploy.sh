a4922002aa615bac09ce13ec1fb92e4e36056f2f
onassis <onxssis@gmail.com>
2021-02-14 03:31:26 +0100

    add cloudformation

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 echo "Creating CloudFormation Stack..."
 
-travis_wait aws cloudformation deploy --template-file cloudformation.yaml --capabilities CAPABILITY_NAMED_IAM --stack-name v1commerce-eks-stack
+aws cloudformation deploy --template-file cloudformation.yaml --capabilities CAPABILITY_NAMED_IAM --stack-name v1commerce-eks-stack
