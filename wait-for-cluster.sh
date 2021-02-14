9c4e977649ba7aa8c2dc3a29fb5200bb08a7a46b
onassis <onxssis@gmail.com>
2021-02-14 01:33:17 +0100

    add cloudformation

--- wait-for-cluster.sh
+++ wait-for-cluster.sh
@@ -1,11 +1,13 @@
+echo "Waiting for CloudFormation Stack to Create/Update..."
+
 started_date=$(date '+%H:%M:%S')
 start=$(date +%s)
 while true; do
-  if [[ $(aws cloudformation describe-stacks --region $AWS_REGION --stack-name v1commerce-eks-stack --query "Stacks[*].StackStatus" --output text) == CREATE_IN_PROGRESS ]]; then
-    echo -e "EKS Cluster status : CREATE IN PROGRESS \n"
+  if [[ $(aws cloudformation describe-stacks --region $AWS_REGION --stack-name v1commerce-eks-stack --query "Stacks[*].StackStatus" --output text) == *"PROGRESS"* ]]; then
+    echo -e "EKS Cluster status : $(!!) \n"
     sleep 10
-  elif [[ $(aws cloudformation describe-stacks --region $AWS_REGION --stack-name v1commerce-eks-stack --query "Stacks[*].StackStatus" --output text) == CREATE_COMPLETE ]]; then
-    echo -e "EKS Cluster status : SUCCESSFULLY CREATED \n"
+  elif [[ $(aws cloudformation describe-stacks --region $AWS_REGION --stack-name v1commerce-eks-stack --query "Stacks[*].StackStatus" --output text) == *"COMPLETE"* ]]; then
+    echo -e "EKS Cluster status : $(!!) \n"
     end=$(date +%s)
     runtime=$((end - start))
     finished_date=$(date '+%H:%M:%S')
